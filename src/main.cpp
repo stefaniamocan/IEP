90efb97c8552dbc1843f932a86da8fb1c95f12bb
stefaniamocan <stefania_mocan@yahoo.com>
2021-12-29 10:28:12 +0200

    "First homework"

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <Pen.hpp>
+#include <Paper.hpp>
+#include <Book.hpp>
+#include <PaintingBook.hpp>
+int main()
+{
+  
+  //Pen p;
+  //std::cout<<p.getRubber()<<std::endl;
+
+  //Pen p2(p);
+ // Pen p3;
+  //p3=p;
+
+  //Paper pa;
+
+  //Paper pa2(pa); //private constructor
+
+ 
+
+  return 0;
+}
